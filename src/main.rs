1850352bd245afa40aaf7a597f0bfb2be803dcfb
alexei0kazachina <alexei.kazachina@yandex.com>
2022-11-07 16:15:49 +0200

    изменено:      src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,17 @@ fn main() {
 		for arg in std::env::args().skip(1) {
 				numbers.push(u64::from_str(&arg)
 				
- .expect("error parsing argument"));
+ .expect("ошибка синтаксического анализа аргумента"));
 		}
 		if numbers.len() == 0 {
-				writeln!(std::io::stderr(), "Usage: gcd NUMBER ...").unwrap();
+				writeln!(std::io::stderr(), "Использование: gcd NUMBER ...").unwrap();
 				std::process::exit(1);
 		}
 		let mut d = numbers[0];
 		for m in &numbers[1..] {
 				d = gcd(d, *m);
 		}
-		println!("The greatest common divisor of {:?} is {}",
+		println!("Наибольший общий делитель из {:?} is {}",
 		
  numbers, d);
 }
