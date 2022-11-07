e58e90fed2c67c58d60e9e61d71e0a5e54a0172b
alexei0kazachina <alexei.kazachina@yandex.com>
2022-11-07 15:37:38 +0200

    изменено:      src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
  fn main() {
-    println!("Hello, world!");
+    println!("evklid");
 }
 fn gcd(mut n: u64, mut m: u64) -> u64 {
     assert!(n != 0 && m != 0);
@@ -13,3 +13,12 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
     }
     n
     }
+    #[test]
+fn test_gcd() {
+		assert_eq!(gcd(14, 15), 1);
+		assert_eq!(gcd(2 * 3 * 5 * 11 * 17,
+		
+ 3 * 7 * 11 * 13 * 19),
+		
+ 3 * 11);
+}
